0dee536999f5d08ca5a39f5573a77cb830693624
cosmasnwigbo <cosmasnwigbo@gmail.com>
2024-03-13 02:29:26 -0700

    a+b

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# print_last_digit(number) - a function that prints the last digit of a number
+# @number parameter of function print_last_digit
+
+
+def print_last_digit(number):
+    number =  number % 10
+    return (number)
