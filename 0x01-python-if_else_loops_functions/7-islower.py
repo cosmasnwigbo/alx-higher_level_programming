48d3361ba7f5ff63f4ff76a072cc2c55f182fa94
cosmasnwigbo <cosmasnwigbo@gmail.com>
2024-03-12 23:47:56 -0700

    islower function implementation

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-#def islower(c) - a function that checks for lowercase character
-#@c: parameter of function c
-#Return: True otherwise False
+# def islower(c) - a function that checks for lowercase character
+# @c: parameter of function c
+# Return: True otherwise False
 
 
 def islower(c):
@@ -11,4 +11,3 @@ def islower(c):
     elif(ord('a') <= ord(c) <= ord('z')):
         return(True)
     return(False)
-
