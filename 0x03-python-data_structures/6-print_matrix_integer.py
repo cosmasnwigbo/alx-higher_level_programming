0a8c9ebac12c747d508718488eb644728530a86c
cosmasnwigbo <cosmasnwigbo@gmail.com>
2024-03-17 10:47:12 -0700

    print matrix integer

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# print_matrix_integer -  a function that prints a matrix of integers
+# @matrix: parameter of print_matrix_integer function
+
+
+def print_matrix_integer(matrix=[[]]):
+    for x in matrix:
+        for y in x:
+            print(f"{y}", end=" ")
+        print('\n')
