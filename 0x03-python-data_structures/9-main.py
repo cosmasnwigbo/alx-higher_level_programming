b8324360663413de91ff698fad534247f952da5c
cosmasnwigbo <cosmasnwigbo@gmail.com>
2024-03-18 17:56:00 -0700

    -python-data_structures project

--- 0x03-python-data_structures/9-main.py
+++ 0x03-python-data_structures/9-main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 max_integer = __import__('9-max_integer').max_integer
 
-my_list = []
+my_list = [-1, -400, -5, -100, -5]
 max_value = max_integer(my_list)
 print("Max: {}".format(max_value))
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -7,6 +7,9 @@ def max_integer(my_list=[]):
         return (None)
     else:
         for x in range(len(my_list)):
-            if my_list[x] > mx:
+            if my_list[x] >= 0 and my_list[x] >  mx:
                 mx = my_list[x]
+            elif my_list[x] < 0:
+                if mx > my_list[x]:
+                    mx = my_list[x]
     return (mx)
