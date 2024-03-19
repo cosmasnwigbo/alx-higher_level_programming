70b53978a6c58797f34f2a879309422f881ad02a
cosmasnwigbo <cosmasnwigbo@gmail.com>
2024-03-18 17:07:48 -0700

    -python-data_structures project

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -7,5 +7,5 @@
 def print_matrix_integer(matrix=[[]]):
     for x in matrix:
         for y in x:
-            print("{:d}".format(x), end=" ")
+            print("{:d}".format(y), end=" ")
         print('\n')
