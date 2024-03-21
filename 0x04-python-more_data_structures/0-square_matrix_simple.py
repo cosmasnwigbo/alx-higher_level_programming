86f07ce262d475e0c51bf8b766ec870c5ba4b17e
cosmasnwigbo <cosmasnwigbo@gmail.com>
2024-03-21 12:49:13 -0700

    0x04-python-more_data_structures

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+
+    for x in matrix:
+        row = []
+        for y in x:
+            row.append(y ** 2)
+        new_matrix.append(row)
+    return (new_matrix)
+
+
