1bb1f41dfd235b66c071b4d3b663077b8a596bee
cosmasnwigbo <cosmasnwigbo@gmail.com>
2024-04-18 02:25:09 -0700

    update

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+A module documentation
+"""
+
+
+class Square:
+    """
+    This class square definition takes a private attribute size
+    """
+    def __init__(self, size = 0):
+        self.__size = size
+
