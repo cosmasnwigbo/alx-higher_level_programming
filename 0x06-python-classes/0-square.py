ee81294ba0df6ea8b77f4f5eb47303f4b8862bde
cosmasnwigbo <cosmasnwigbo@gmail.com>
2024-04-18 01:38:10 -0700

    update

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 "
-An empty class Square that defines a square
+A documentation for an empty class Square that defines a square
 "
 
 class Square:
+     """ This is a class that does nothing but defines a class """
     pass
